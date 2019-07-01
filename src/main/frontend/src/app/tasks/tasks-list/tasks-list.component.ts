25726d6d09da4e007dc34eef6237e8746fc7232e
Marc Holman <marcholman9323@gmail.com>
2019-07-01 16:42:03 -0500

    working out bootstrap 3 -> 4 conversion

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Task} from "./task.model";
 
 @Component({
   selector: 'app-tasks-list',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
+    tasks: Task[] = [];
 
   constructor() { }
 
   ngOnInit() {
+      this.tasks.push(new Task(1, "Task 1", true, "07/27/73"));
+      this.tasks.push(new Task(1, "Task 2", false, "07/27/73"));
+      this.tasks.push(new Task(1, "Task 3", true, "07/27/73"));
+  }
+
+  getDueDateLabel(task: Task){
+      return task.completed ? '' : 'alert alert-danger'
+  }
+
+  onTaskChange(event, task) {
+      //this.taskService.saveTask(task, event.target.checked).subscribe();
+      console.log("task has changed");
   }
 
 }
